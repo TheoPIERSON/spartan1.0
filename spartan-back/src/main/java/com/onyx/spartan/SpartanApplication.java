f98ccfdbf76e00398a7acfa5322d8a896da90a28
Theo Pierson <thheopierson@gmail.com>
2023-10-25 16:57:01 +0200

    feat: create java spring boot project and configure dockerfile

---
+++ spartan-back/src/main/java/com/onyx/spartan/SpartanApplication.java
@@ -0,0 +1,20 @@
+package com.onyx.spartan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpartanApplication {
+	@RequestMapping("/")
+	public String home(){
+		return "Hello Java";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartanApplication.class, args);
+	}
+
+}
