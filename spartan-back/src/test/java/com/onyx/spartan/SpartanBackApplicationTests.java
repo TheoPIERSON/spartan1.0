1e47ff6dda4f72a0d68f37049c95e0c573c9f7af
Theo Pierson <thheopierson@gmail.com>
2023-10-25 13:55:20 +0200

    feat: create spartan-back folder with java project

---
+++ spartan-back/src/test/java/com/onyx/spartan/SpartanBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onyx.spartan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartanBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
