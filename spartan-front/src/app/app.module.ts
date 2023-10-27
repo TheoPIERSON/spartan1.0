aa297ce82f466760b6e42e9770721fa9bddb26d4
Theo Pierson <thheopierson@gmail.com>
2023-10-27 14:40:55 +0200

    feat: create first component for client app

--- spartan-front/src/app/app.module.ts
+++ spartan-front/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HelloclientComponent } from './helloclient/helloclient.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, HelloclientComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ spartan-front/src/app/helloclient/helloclient.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-helloclient',
+  templateUrl: './helloclient.component.html',
+  styleUrls: ['./helloclient.component.css']
+})
+export class HelloclientComponent {
+
+}
