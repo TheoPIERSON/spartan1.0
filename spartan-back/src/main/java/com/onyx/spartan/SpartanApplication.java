eb3b22e143314d42358ee92b52ec306f3b7cb348
Theo Pierson <thheopierson@gmail.com>
2023-10-27 13:39:48 +0200

    feat: create backend app and connect database to postgres docker image

--- spartan-back/src/main/java/com/onyx/spartan/SpartanApplication.java
+++ spartan-back/src/main/java/com/onyx/spartan/SpartanApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpartanApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpartanApplication.class, args);
-	}
-
+public static void main(String[] args) {
+	SpringApplication.run(SpartanApplication.class, args);
+    }
 }
