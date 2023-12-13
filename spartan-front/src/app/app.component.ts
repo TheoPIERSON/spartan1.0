e1f85ec038dc6138423ff24458deaa42e78208db
Theo Pierson <thheopierson@gmail.com>
2023-12-13 15:15:48 +0100

    feat: update global css

--- spartan-front/src/app/app.component.ts
+++ spartan-front/src/app/app.component.ts
@@ -1,26 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpService } from './http.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  title = 'Coaching-Front';
-  isLoading = false;
-  text: string = '';
-  constructor(private httpService: HttpService) {}
-
-  ngOnInit(): void {}
-
-  testAction() {
-    this.text = '';
-    this.httpService.getHello().subscribe(async (result) => {
-      this.isLoading = true;
-      await new Promise((f) => setTimeout(f, 3000));
-      this.isLoading = false;
-      this.text = result;
-    });
-  }
+export class AppComponent {
+  title = 'SpartanFrontend';
 }
--- spartan-front/src/app/app.module.ts
+++ spartan-front/src/app/app.module.ts
@@ -3,18 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerService } from './core/services/customer.service';
 import { HttpClientModule } from '@angular/common/http';
+import { CustomerCardComponent } from './customer-card/customer-card.component';
+import { CustomerSearchComponent } from './customer-search/customer-search.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CustomerAddComponent } from './customer-add/customer-add.component';
+import { CustomerSearchListComponent } from './customer-search-list/customer-search-list.component';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ModalComponent } from './modal/modal.component';
+import { CustomerIdService } from './core/services/customer-id.service';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    CustomerComponent,
+    CustomerCardComponent,
+    CustomerSearchComponent,
+    SidebarComponent,
+    CustomerAddComponent,
+    CustomerSearchListComponent,
+    ModalComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
     BrowserAnimationsModule,
+    MatButtonModule,
+    MatDialogModule,
   ],
-  providers: [],
+  providers: [CustomerService, CustomerIdService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
