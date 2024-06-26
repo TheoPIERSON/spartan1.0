3c9579f7d9cffbf08b552f4241a66035cee6f17c
Theo Pierson <thheopierson@gmail.com>
2024-06-26 15:25:46 +0200

    feat: remove fakelogin component and add login component

--- spartan-front/src/app/pages/login/login.component.spec.ts
+++ spartan-front/src/app/pages/login/login.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FakeloginComponent } from './fakelogin.component';
+import { LoginComponent } from './login.component';
 
-describe('FakeloginComponent', () => {
-  let component: FakeloginComponent;
-  let fixture: ComponentFixture<FakeloginComponent>;
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FakeloginComponent]
+      declarations: [LoginComponent]
     });
-    fixture = TestBed.createComponent(FakeloginComponent);
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
